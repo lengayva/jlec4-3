3fea8b9535f82e998a7e78cc27988cad6749f3f0
Elena Kovylyaeva <lengayva@yandex.ru>
2023-03-26 16:12:05 +0500

    add CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int creditAmountRub, int creditPeriodYears, double rateOfInterest) {
-        double monthlyPayment = creditAmountRub * (((rateOfInterest / 100 / 12) * Math.pow(1 + (rateOfInterest / 100 / 12), creditPeriodYears * 12)) / (Math.pow((1 + (rateOfInterest / 100 / 12)), creditPeriodYears * 12) - 1));
-        return (int) monthlyPayment;
+        return (int) (creditAmountRub * (((rateOfInterest / 100 / 12) * Math.pow(1 + (rateOfInterest / 100 / 12), creditPeriodYears * 12)) / (Math.pow((1 + (rateOfInterest / 100 / 12)), creditPeriodYears * 12) - 1)));
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,15 @@ public class Main {
         int creditPeriodYears = 1;
         double rateOfInterest = 9.99;
         int monthlyPayment = service.calculate(creditAmountRub, creditPeriodYears, rateOfInterest);
-        System.out.println(monthlyPayment);
+        System.out.println("1млн, 1 год, 9,99% :");
+        System.out.println(monthlyPayment + " руб");
+        System.out.println();
+
+        System.out.println("1млн, 2 года, 9,99% :");
+        System.out.println(service.calculate(1_000_000, 2, 9.99) + " руб");
+        System.out.println();
+
+        System.out.println("1млн, 3 года, 9,99% :");
+        System.out.println(service.calculate(1_000_000, 3, 9.99) + " руб");
     }
 }
